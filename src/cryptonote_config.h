f4d5e14083bcdc64e22245194139e4abd98f15d3
IdotMaster1 <justapersom@gmail.com>
2022-04-21 00:15:17 +0200

    Change genesis and peer magic with super secret ANSI

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -156,9 +156,9 @@
 
 #define RPC_IP_FAILS_BEFORE_BLOCK                       3
 
-#define CRYPTONOTE_NAME                         "bitmonero"
-#define CRYPTONOTE_BLOCKCHAINDATA_FILENAME      "data.mdb"
-#define CRYPTONOTE_BLOCKCHAINDATA_LOCK_FILENAME "lock.mdb"
+#define CRYPTONOTE_NAME                         "bitshmingus"
+#define CRYPTONOTE_BLOCKCHAINDATA_FILENAME      "blkchaindata.mdb"
+#define CRYPTONOTE_BLOCKCHAINDATA_LOCK_FILENAME "blkchainlock.mdb"
 #define P2P_NET_DATA_FILENAME                   "p2pstate.bin"
 #define RPC_PAYMENTS_DATA_FILENAME              "rpcpayments.bin"
 #define MINER_CONFIG_FILE_NAME                  "miner_conf.json"
@@ -219,10 +219,10 @@ namespace config
   uint16_t const RPC_DEFAULT_PORT = 18081;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 18082;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
+      0x42, 0x75, 0x6E, 0x6B, 0x65, 0x72, 0x63, 0x6F, 0x69, 0x6E, 0x74, 0x6F, 0x20, 0x6D, 0x6F, 0x6E // mon = moon. 100% not ANSI, 100%...
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
-  uint32_t const GENESIS_NONCE = 10000;
+  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121014a7a340fbdf85f74e078367e1a8fb7d0cf0596fc2723d904f7e6bedd050d4a5a";
+  uint32_t const GENESIS_NONCE = 420;
 
   // Hash domain separators
   const char HASH_KEY_BULLETPROOF_EXPONENT[] = "bulletproof";
